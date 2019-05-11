4305393a4b3ea11925383d5b33dd42ec8f5be969
Insung Jung <superisis@gmail.com>
2019-05-11 11:43:33 +0900

    액티비티에서 프래그먼트 교체 연습 - 버그 수정 필요

--- app/src/main/java/kr/insungjung/fragmentpractice01/MainActivity.java
+++ app/src/main/java/kr/insungjung/fragmentpractice01/MainActivity.java
@@ -1,10 +1,15 @@
 package kr.insungjung.fragmentpractice01;
 
 import android.databinding.DataBindingUtil;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.insungjung.fragmentpractice01.databinding.ActivityMainBinding;
+import kr.insungjung.fragmentpractice01.fragments.FragmentTwo;
 
 public class MainActivity extends BaseActivity {
 
@@ -23,6 +28,18 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        /*프래그먼트 2번 버튼 클릭*/
+        act.changeFragTwoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fr = new FragmentTwo();
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragOne, fr);
+                fragmentTransaction.commit();
+            }
+        });
     }
 
     @Override
