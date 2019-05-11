36597ce746ccae30b1c7cc260eb8311f7fd8f0ac
Insung Jung <superisis@gmail.com>
2019-05-11 10:20:26 +0900

    Initial commit

---
+++ app/src/main/java/kr/insungjung/fragmentpractice01/MainActivity.java
@@ -0,0 +1,36 @@
+package kr.insungjung.fragmentpractice01;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.insungjung.fragmentpractice01.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_main); // bindVeiws() 내부로 이동
+
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
